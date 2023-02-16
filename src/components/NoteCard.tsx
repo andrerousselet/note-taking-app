e2d97f543c1a69ff037e2116749ddf8cffa96724
Andre Rousselet <andre098@hotmail.com>
2023-02-16 15:58:09 -0300

    feat(card body): add title and tags info to card body

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Badge, Card, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { SimplifiedNote } from "../types/NoteTypes";
 import styles from "../styles/NoteCard.module.css";
@@ -11,7 +11,16 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
       className={`h-100 text-reset text-decoration-none ${styles.card}`}
     >
       <Card.Body>
-
+        <Stack gap={2} className="align-items-center justify-content-center h-100">
+          <span className="fs-5">{title}</span>
+          {tags.length && (
+            <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+              {tags.map(tag => (
+                <Badge key={tag.id} className="text-truncate">{tag.label}</Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
       </Card.Body>
     </Card>
   )
