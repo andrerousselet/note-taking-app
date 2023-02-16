0305d9408601d9983521abb06d57296c93adb62b
Andre Rousselet <andre098@hotmail.com>
2023-02-16 15:12:18 -0300

    feat(note list): add NoteList component receiving tags from app as props

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NewNote from "./components/NewNote"
 import useLocalStorage from "./hooks/useLocalStorage";
 import { NoteData, RawNote, Tag } from "./types/NoteTypes";
 import { v4 as uuidv4 } from "uuid";
+import NoteList from "./components/NoteList";
 
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
@@ -29,7 +30,7 @@ function App() {
   return (
     <Container className="my-4">
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<NoteList availableTags={tags} />} />
         <Route
           path="/new"
           element={
