8917c1221570604770c34bde5d4db218de4ae156
Andre Rousselet <andre098@hotmail.com>
2023-02-17 19:14:58 -0300

    refactor(tags provider): wrap app with tags provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import NotesProvider from './contexts/notes/NotesProvider'
+import TagsProvider from './contexts/tags/TagsProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <NotesProvider>
-        <App />
+        <TagsProvider>
+          <App />
+        </TagsProvider>
       </NotesProvider>
     </BrowserRouter>
   </React.StrictMode>,
