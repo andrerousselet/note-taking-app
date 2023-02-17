4b5bb07868eb94852a24f046bf7e81c23e39cde9
Andre Rousselet <andre098@hotmail.com>
2023-02-17 17:38:27 -0300

    refactor(use notes hook): return only context instead of overwriting notes array with empty []

--- src/contexts/notes/useNotes.tsx
+++ src/contexts/notes/useNotes.tsx
@@ -1,8 +1,6 @@
 import { useContext } from "react";
-import { RawNote } from "../../types/NoteTypes";
 import NotesContext from "./NotesContext";
 
-export default function useNotesContext() {
-  const notesContext = useContext(NotesContext);
-  return { ...notesContext, notes: [] as RawNote[] }
+export default function useNotes() {
+  return useContext(NotesContext);
 }
