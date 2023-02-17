bd0b22925d8370a4b50e518cde7fa25f18cd5b3c
Andre Rousselet <andre098@hotmail.com>
2023-02-17 17:41:07 -0300

    feat(notes provider): wrap app with notes provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import NotesProvider from './contexts/notes/NotesProvider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NotesProvider>
+        <App />
+      </NotesProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
